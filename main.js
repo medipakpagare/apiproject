0643a643fdd01e152593d35ae2f1880dd808b2b4
dipak <dipakpagare11@gmail.com>
2021-08-24 16:56:40 +0530

    UI changes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const pokedex = document.getElementById("pokedex");
 
 const fetchPokemon = () => {
   const promises = [];
-  for (i = 1; i <= 50; i++) {
+  for (i = 1; i <= 20; i++) {
     const url = `https://pokeapi.co/api/v2/pokemon/${i}`
     promises.push(fetch(url).then((res) => res.json()));
   }
